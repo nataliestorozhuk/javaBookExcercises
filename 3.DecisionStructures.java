93f102b269df4fff18819be641e7a34350c97f1d
Nata <natalie.storozhuk@outlook.com>
2021-03-15 11:08:19 -0400

    added converting if else statement into swich

--- 3.DecisionStructures.java
+++ 3.DecisionStructures.java
@@ -6,7 +6,8 @@ class DecisionStructures {
         // ifElseStatement();
         // ifElseStatement1();
         // ifElseStatement2();
-        ifElseStatement3();
+        // ifElseStatement3();
+        convertingIfElseIntoSwich();
     }
 
     public static void ifElseStatement() {
@@ -59,5 +60,26 @@ class DecisionStructures {
         }
     }
 
+    public static void convertingIfElseIntoSwich(){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please select numbers from 1-4: ");
+        int number = keyboard.nextInt();
+
+        switch (number){
+            case 1:
+            System.out.println("You selected 1. ");
+            break;
+            case 2, 3:
+            System.out.println("You selected 2 or 3. ");
+            break;
+            case 4:
+            System.out.println("You selected 4. ");
+            break;
+            default:
+            System.out.println("Select again please. ");
+
+        }
+
+    }
 
 }
