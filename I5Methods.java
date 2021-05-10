c5b6726d1d2fb83c4a28d1a6d799956511b70e68
Nata <natalie.storozhuk@outlook.com>
2021-05-10 13:29:11 -0400

    added show char method exersice

---
+++ I5Methods.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.sql.Date;
+import java.time.LocalTime;
+import java.util.*;
+
+public class I5Methods {
+
+    public static void run() {
+
+        showCharMethod("oleksandra", 7);
+
+    }
+
+    public static void showCharMethod(String name, int num) {
+        System.out.println(name.charAt(num));
+
+    }
+}
