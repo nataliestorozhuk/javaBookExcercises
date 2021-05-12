56e9a4494fe9308178a0802b8fb29426f50ed3fb
Nata <natalie.storozhuk@outlook.com>
2021-05-12 11:12:27 -0400

    added retail price calculator exercise

--- I5Methods.java
+++ I5Methods.java
@@ -14,12 +14,31 @@ public class I5Methods {
 
     public static void run() {
 
-        showCharMethod("oleksandra", 7);
+        // showCharMethod("oleksandra", 7);
+        retailPriceCalculator();
 
     }
 
     public static void showCharMethod(String name, int num) {
         System.out.println(name.charAt(num));
 
+    }
+
+    public static void retailPriceCalculator() {
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter item's wholesale cost: ");
+        double wholeSaleCost = keyboard.nextInt();
+        System.out.println("Please enter item's wholesale cost markup percentage: ");
+        double markupPersantage = keyboard.nextInt();
+        double markUpPersantageConv = markupPersantage/100;
+        double retailPrice = calculateRetail(wholeSaleCost, markUpPersantageConv);
+        System.out.println("The retail price of the item is: " + calculateRetail(wholeSaleCost, markUpPersantageConv));
+
+    }
+
+    public static double calculateRetail(double wholesale, double markupPersantage) {
+        double retailPrice = (wholesale * markupPersantage) + wholesale;
+        return retailPrice;
     }
 }
